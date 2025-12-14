a6ae43536288fc19c1bc3e5041d1a8aee3ec2583
Rickson Henrique <rickson.henrique2018@gmail.com>
2025-12-14 16:47:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6670819864-d3766",
+  "appId": "1:543656462486:web:539cee9985c0f3000ddab7",
+  "apiKey": "AIzaSyDpo363tr4kDp-5P_99nRT0QHodOQtM-yA",
+  "authDomain": "studio-6670819864-d3766.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "543656462486"
+};
