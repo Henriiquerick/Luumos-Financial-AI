edc4390d4e9bd7004ef26d25c1a198957b5b91dc
Rickson Henrique <rickson.henrique2018@gmail.com>
2025-12-14 17:43:19 +0000

    Tarefa: Correção de Tipagem no Modal de Edição (Closing Day)
    Problema:

--- src/components/card-form.tsx
+++ src/components/card-form.tsx
@@ -160,6 +160,7 @@ export function CardForm({ onSave, cardToEdit }: CardFormProps) {
                     ))}
                   </SelectContent>
                 </Select>
+                <FormMessage />
               </FormItem>
             )}
           />
