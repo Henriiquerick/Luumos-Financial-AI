2ee3123f8033853c090b62d02bdb589e92c468c5
Rickson Henrique <rickson.henrique2018@gmail.com>
2025-12-17 02:30:21 +0000

    mudança no modelo

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,7 +3,5 @@ import { googleAI } from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  // TRUQUE DE MESTRE: Usamos o nome completo com prefixo.
-  // O plugin 'google-genai' registra os modelos com o prefixo 'googleai'.
   model: 'googleai/gemini-1.5-flash', 
 });
