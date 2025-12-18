dec2117e3bfd13baf1d172bd4d8d3bf76857b4a5
Rickson Henrique <rickson.henrique2018@gmail.com>
2025-12-18 18:12:02 +0000

    Passo 1: Limpe o next.config.ts (ou .js) Abra o arquivo next.config.ts n

--- next.config.ts
+++ next.config.ts
@@ -8,58 +8,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https,',
-        hostname: 'seeklogo.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.vectorlogo.zone',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'img.icons8.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
-  },
 
   /* --- Correção para Firebase Auth (Popup Closed by User) --- */
   async headers() {
