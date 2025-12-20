db3ffefc2ec66070ba8eeebcc29d46924f3869a5
Rickson Henrique <rickson.henrique2018@gmail.com>
2025-12-20 06:55:50 +0000

    Vamos fazer o seguinte: Passo 1 : Troque a chave principal do Gemini por

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,5 +4,5 @@ import { googleAI } from '@genkit-ai/google-genai';
 export const ai = genkit({
   plugins: [googleAI()],
   // Alterado para o modelo Flash para ter uma cota mais permissiva durante o desenvolvimento.
-  model: 'googleai/gemini-2.5-flash-preview', 
+  model: 'googleai/gemini-2.0-flash', 
 });
